1738951783457f7772524f884d1565b02b274ad1
doscanner <liaoweishiye@163.com>
2015-11-18 16:56:48 +0800

    1

--- GeneralFramework/GF.Domain.ViewModel/Properties/AssemblyInfo.cs
+++ GeneralFramework/GF.Domain.ViewModel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("GF.ViewModel")]
+[assembly: AssemblyTitle("GF.Domain.ViewModel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("GF.ViewModel")]
+[assembly: AssemblyProduct("GF.Domain.ViewModel")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("8a8e6b5c-b459-4ad5-b13b-8dbf1f9df1e1")]
+[assembly: Guid("a87c14d3-c078-47e5-bfbc-4cc6040ff436")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
